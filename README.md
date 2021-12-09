f58a3d93134cc27c9440bfa24cb4bf66d49daaeb
monosans <hsyqixco@protonmail.com>
2021-12-09 23:46:49 +0300

    v0.1.0

---
+++ README.md
@@ -0,0 +1,5 @@
+# pygismeteo-base
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://github.com/monosans/pygismeteo-base/blob/main/LICENSE)
+
+База для [pygismeteo](https://github.com/monosans/pygismeteo) и [aiopygismeteo](https://github.com/monosans/aiopygismeteo).
---
+++ pygismeteo_base/dates/ten_days.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from typing import Dict
+
+from pygismeteo_base.dates import xpaths
+from pygismeteo_base.dates.two_weeks import TwoWeeksBase
+
+
+class TenDays(TwoWeeksBase):
+    """Возвращается методом ten_days() класса Gismeteo."""
+
+    @property
+    def ultraviolet_index(self) -> Dict[str, str]:
+        """Ультрафиолетовый индекс, баллы."""
+        return self._build_result(*xpaths.ULTRAVIOLET_INDEX)
+
+    @property
+    def falling_snow(self) -> Dict[str, str]:
+        """Выпадающий снег, см."""
+        return self._build_result(*xpaths.FALLING_SNOW)
+
+    @property
+    def snow_depth(self) -> Dict[str, str]:
+        """Высота снежного покрова, см."""
+        return self._build_result(*xpaths.SNOW_DEPTH)
