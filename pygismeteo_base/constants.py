93603e5434bb1bb1331c49f2244c8b7fe98603e7
monosans <hsyqixco@protonmail.com>
2021-12-10 20:15:53 +0300

    Add URL_REGEX

--- pygismeteo_base/constants.py
+++ pygismeteo_base/constants.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+URL_REGEX = r".*(weather-.*-\d+).*"
 USER_AGENT = (
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
     + "(KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"
