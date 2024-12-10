1555675c66f69335b95e35fb8f800152f18f6db1
monosans <hsyqixco@protonmail.com>
2024-12-10 14:57:21 +0300

    hotfix

--- pygismeteo_base/responses.py
+++ pygismeteo_base/responses.py
@@ -12,23 +12,21 @@ class _CurrentResponse(TypedDict):
     response: models.current.Model
 
 
-current: Final = TypeAdapter(_CurrentResponse, config={"frozen": True})
+current: Final = TypeAdapter(_CurrentResponse)
 
 
 class _SearchByCoordinates(TypedDict):
     response: models.search_by_coordinates.Model
 
 
-search_by_coordinates: Final = TypeAdapter(
-    _SearchByCoordinates, config={"frozen": True}
-)
+search_by_coordinates: Final = TypeAdapter(_SearchByCoordinates)
 
 
 class _SearchByIp(TypedDict):
     response: models.search_by_ip.Model
 
 
-search_by_ip: Final = TypeAdapter(_SearchByIp, config={"frozen": True})
+search_by_ip: Final = TypeAdapter(_SearchByIp)
 
 
 class _SearchByQueryItems(TypedDict):
@@ -39,25 +37,25 @@ class _SearchByQuery(TypedDict):
     response: _SearchByQueryItems
 
 
-search_by_query: Final = TypeAdapter(_SearchByQuery, config={"frozen": True})
+search_by_query: Final = TypeAdapter(_SearchByQuery)
 
 
 class _Step3(TypedDict):
     response: models.step3.Model
 
 
-step3: Final = TypeAdapter(_Step3, config={"frozen": True})
+step3: Final = TypeAdapter(_Step3)
 
 
 class _Step6(TypedDict):
     response: models.step6.Model
 
 
-step6: Final = TypeAdapter(_Step6, config={"frozen": True})
+step6: Final = TypeAdapter(_Step6)
 
 
 class _Step24(TypedDict):
     response: models.step24.Model
 
 
-step24: Final = TypeAdapter(_Step24, config={"frozen": True})
+step24: Final = TypeAdapter(_Step24)
